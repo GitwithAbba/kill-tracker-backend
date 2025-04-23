7ab2988234cc02307481ad4d2efc8ce2c615e954
GitWithAbba <mlz82591@gmail.com>
2025-04-22 20:31:12 -0400

    updated file

--- models.py
+++ models.py
@@ -25,6 +25,23 @@ class KillEventModel(Base):
     organization_url = Column(String, nullable=True)
 
 
+class DeathEventModel(Base):
+    __tablename__ = "deaths"
+    id = Column(Integer, primary_key=True, index=True)
+    killer = Column(String, nullable=False)
+    victim = Column(String, nullable=False)
+    time = Column(DateTime, nullable=False)
+    zone = Column(String, nullable=False)
+    weapon = Column(String, nullable=False)
+    damage_type = Column(String, nullable=False)
+    rsi_profile = Column(String, nullable=False)
+    game_mode = Column(String, nullable=False)
+    killers_ship = Column(String, nullable=False)
+    avatar_url = Column(String, nullable=True)
+    organization_name = Column(String, nullable=True)
+    organization_url = Column(String, nullable=True)
+
+
 class APIKey(Base):
     __tablename__ = "api_keys"
     key = Column(String, primary_key=True, index=True)
